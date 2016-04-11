7e97d8d364064c21f1fa60a9c888a85013692f1e
andrey-zhiryakov <andrey.zhiriakov@gmail.com>
2016-04-12 01:30:25 +0300

    add reverse, each functions

--- linked-list/task.js
+++ linked-list/task.js
@@ -1,6 +1,12 @@
 'use strict'
-
+debugger;
 //double linked list
+function Node(data, prev, next) {
+  this.data = data ? data : null;
+  this.prev = prev ? prev : null;
+  this.next = next ? next : null;
+}
+
 function List(){
   this.listHead = null;
   this.listTail = null;
@@ -8,19 +14,15 @@ function List(){
 
 List.prototype = {
   head : function() {
-    return this.listHead;
+    return this.listHead.data;
   },
 
   tail : function() {
-    return this.listTail;
+    return this.listTail.data;
   },
 
   append : function(data) {
-    var node = {
-      next: null,
-      prev: null,
-      data: data
-    };
+    var node = new Node(data);
 
     if (this.listHead === null) {
       this.listHead = node;
@@ -35,7 +37,7 @@ List.prototype = {
     return this;
   },
 
-  at : function(index) {
+  at : function(index, isReturnNode) {
     if (typeof index != 'number' || index < 0) {
       return undefined;
     }
@@ -49,14 +51,52 @@ List.prototype = {
       }
 
     }
-    return item;
+    return isReturnNode ? item : item.data;
   },
-  insertAt : function(index, data) {
-    var currentItem = this.at(index);
+  insertAt : function(index, data) { //complete with given the fact, that element == head or tail
+    var currentItem = this.at(index, true);
+    if (currentItem) {
+      var newItem = new Node(data, currentItem.prev, currentItem);
+      currentItem.prev.next = newItem;
+      currentItem.prev = newItem;
+    }
+    return this;
   },
-  deleteAt : function(index) {},
-  reverse : function() {},
-  each : function(func) {},
+  deleteAt : function(index) {
+    var itemToDelete = this.at(index, true);
+    if (itemToDelete) {
+      itemToDelete.prev.next = itemToDelete.next;
+      itemToDelete.next.prev = itemToDelete.prev;
+    }
+    return this;
+  },
+  reverse : function() {
+    debugger;
+    if (this.listHead === null) {
+      return;
+    }
+
+    var oldHead = this.listHead;
+    this.listHead = this.listTail;
+    this.listTail = oldHead;
+
+    var currentItem = this.listHead;
+    while (currentItem) {
+        var oldPrev = currentItem.prev, oldNext = currentItem.next;
+        currentItem.next = oldPrev;
+        currentItem.prev = oldNext;
+        currentItem = oldPrev;
+    }
+    return this;
+  },
+  each : function(func) {
+    var item = this.listHead, index = 0;
+    while (item) {
+      func(item.data, index++);
+      item = item.next ? item.next : undefined;
+    }
+    return this;
+  },
   indexOf : function(data) {},
   length : function() {
     if (this.listHead) {
@@ -69,19 +109,3 @@ List.prototype = {
     }
   }
 };
-
-
-
-var shit = new List();
-shit.append(2);
-console.log(shit.tail().data);
-
-shit.append(4);
-shit.append(6);
-console.log(shit.tail().data);
-console.log("Длина листа: "+shit.length());
-console.log("--------------------");
-console.log(shit.at(0).data);
-console.log(shit.at(1).data);
-console.log(shit.at(2).data);
-console.log(shit.at(-11).data);
