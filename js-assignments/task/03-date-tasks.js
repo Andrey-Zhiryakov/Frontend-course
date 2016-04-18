05d74adad35beec76358004c7f240249bb32b5e4
andrey-zhiryakov <andrey.zhiriakov@gmail.com>
2016-04-18 23:58:36 +0300

    complete 'data'

--- js-assignments/task/03-date-tasks.js
+++ js-assignments/task/03-date-tasks.js
@@ -22,7 +22,8 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+  //  throw new Error('Not implemented');
+  return new Date(value);
 }
 
 /**
@@ -37,7 +38,8 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+  //  throw new Error('Not implemented');
+  return new Date(value);
 }
 
 
@@ -56,7 +58,22 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+  //  throw new Error('Not implemented');
+  var year = date.getYear() + 1900;
+  if (year % 4 !== 0){
+    return false;
+  } else if (year % 100 !== 0) {
+    return true;
+  } else if (year % 400 !== 0) {
+    return false;
+  } else {
+    return true;
+  }
+
+//   if (year is not divisible by 4) then (it is a common year)
+// else if (year is not divisible by 100) then (it is a leap year)
+// else if (year is not divisible by 400) then (it is a common year)
+// else (it is a leap year)
 }
 
 
@@ -76,14 +93,23 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+  //  throw new Error('Not implemented');
+  var result = endDate - startDate, hours = Math.floor(result / (1000*60*60)),
+      minutes = Math.floor(result / (1000*60) % 60), seconds = Math.floor(result / (1000) % 60),
+      milliseconds = Math.floor(result % 1000);
+
+      function foo (val) {
+        return "00".slice(val.toString().length) + val;
+      }
+  var answer = foo(hours) + ":" + foo(minutes) + ":" + foo(seconds) + ".000".slice(0, -milliseconds.toString().length) + milliseconds;
+  return answer;
 }
 
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock for the specified Greenwich time.
  * If you have problem with solution please read: https://en.wikipedia.org/wiki/Clock_angle_problem
- * 
+ *
  * @param {date} date
  * @return {number}
  *
