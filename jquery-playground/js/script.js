78a18b33693984625451b0d780ffa80a4d205a44
andrey-zhiryakov <andrey.zhiriakov@gmail.com>
2016-04-27 00:58:40 +0300

    complete 'edit' button, make tab count limitation

--- jquery-playground/js/script.js
+++ jquery-playground/js/script.js
@@ -7,18 +7,23 @@ $(document).ready(function(){
   .set(tabs[3] , {'content':'This is uniqe content!', 'date': '', 'img': null});
   tabsContentSize+=4;
 
+  $('#text').val(0);
+  $('#title').val($('.tab').first().text());
+  $('#html').val($('.container').html());
+
   window.s = function(){  //just for debug
     return tabsContent;
   }
 
-  function addButton() {
-
-  }
-
-
   function asideAddMsg(type, message) {
     $.simplyToast(message, type, {appendTo: 'aside', offset : {'from':'bottom'}});
   }
+
+  $('#reset-btn').on('click', (e) => {
+    $('#text').val('');
+    $('#title').val('');
+    $('#html').val('');
+  });
 
   //tabs event handler
   $('.tabs').on('click', function(e){
@@ -27,47 +32,93 @@ $(document).ready(function(){
         $(this).removeClass('active');
         if (el === e.target) {
           $(this).addClass('active');
-          $('.container').html(tabsContent.get(e.target).content);
+          var content = tabsContent.get(e.target).content;
+          $('.container').html(content);
+          $('#text').val(ind);
+          $('#title').val($(this).text());
+          $('#html').val(content);
         }
       });
     };
   }());
+
+  function addItem(item) {
+    var isNotFilled = false;
+
+    $('form input').each(function() {
+      if ($(this).val() === '') {
+        isNotFilled = true;
+        return false;
+      }
+      return true;
+    });
+
+    if ($('#html').val() === ''){
+      isNotFilled = true;
+    }
+
+    if (isNotFilled) {
+      asideAddMsg('danger', 'You should fill all text inputs!');
+      return false;
+    }
+
+    var index = $('#text').val();
+    var title = $('#title').val();
+
+    if (index < 0 || index > tabsContentSize-1) {
+      item.appendTo('.tabs').text(title);
+    } else {
+      item.insertBefore($('.tab').get(index)).text(title);
+    }
+    return true;
+  }
+
+  function tabOffset() { return;
+    $('.tab').each(function (ind, el) {
+      var offset = -20*ind;
+      el.style.left = offset.toString(10) + 'px';
+      return true;
+    });
+  }
 
   //add button event handler
   $('#add-btn').on('click', function() {
     return function(){
-      var isNotFilled = false;
+      var tabsBarWidth = parseInt($('.tabs').css('width'),10),
+      tabWidth = parseInt($('.tab').css('width'),10),
+      tabsCount = $('.tab').length;
 
-      $('form input').each(function() {
-        if ($(this).val() === '') {
-          isNotFilled = true;
-          return false;
-        }
-        return true;
-      });
-
-      if (isNotFilled) {
-        asideAddMsg('danger', 'You should fill all text inputs!');
+      if (tabsCount * tabWidth - (16 * (tabsCount-5))  >= tabsBarWidth) {
+        asideAddMsg('danger', 'Max length has been reached!');
         return;
       }
-
-      var index = $('#text').val();
-      var title = $('#title').val();
-      var content = $('#html').val();
       var item = $('.tab:first').clone();
-
-      if (index > tabsContentSize-1) {
-        item.appendTo('.tabs').text(title).removeClass('active');
-      } else {
-        item.insertBefore($('.tab').get(index)).text(title).removeClass('active');
+      if (!addItem(item)) {
+        return;
       }
+      tabOffset();
+
+      var content = $('#html').val();
+
+      item.removeClass('active');
       tabsContentSize++;
       tabsContent.set(item[0], {'content': content, 'date' : '', 'img' : null});
-
 
       asideAddMsg('success', 'Tab successfully added!');
     };
   }());
+
+  $('#edit-btn').on('click', (e) => {
+    var item = $('.tab.active');
+    if (!addItem(item)) {
+      return;
+    }
+    tabsContent.get(item[0]).content = $('#html').val();
+    var event = new Event("click", {bubbles : true});
+    item[0].dispatchEvent(event);
+    tabOffset();
+    asideAddMsg('success', 'Tab successfully edited!');
+  });
 
   // aside event handler
   $('a.collapse').on('click',function(){
