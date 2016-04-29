cf843f8d836cd55b92de795664100b2f4a86147e
andrey-zhiryakov <andrey.zhiriakov@gmail.com>
2016-04-29 14:36:11 +0300

    fully made slider, fix some mistakes, complete task

--- jquery-playground/js/script.js
+++ jquery-playground/js/script.js
@@ -53,15 +53,16 @@ $(document).ready(function() {
 		$('#title').val('');
 		$('#html').val('');
 		$('#datepicker').val('');
+		$('#image').val('');
 	}
 
 	function addItem(item) {
-		var isNotFilled = false;
+		var isNotFilled = false; //flag for checking filling of inputs
 
 		$('form input').each(function() {
-			if ($(this).val() === '') {
+			if ($(this).val() === '') { // if input isn't filled, set flag to true state
 				isNotFilled = true;
-				return false;
+				return false; //break the iteration
 			}
 			return true;
 		});
@@ -70,7 +71,7 @@ $(document).ready(function() {
 			isNotFilled = true;
 		}
 
-		if (isNotFilled) {
+		if (isNotFilled) { //tell to user that he didn't fill all inputs
 			asideAddMsg('danger', 'You should fill all text inputs!');
 			return false;
 		}
@@ -78,10 +79,10 @@ $(document).ready(function() {
 		var index = $('#text').val(),
 			title = $('#title').val();
 
-		if (index < 0 || index > tabsContentSize - 1) {
+		if (index < 0 || index > tabsContentSize - 1) { // if index out of range, we append child to end of list
 			item.appendTo('.tabs').text(title);
 		} else {
-			item.insertBefore($('.tab').get(index)).text(title);
+			item.insertBefore($('.tab').get(index)).text(title); //else insert before item with received index
 		}
 		return true;
 	}
@@ -112,6 +113,8 @@ $(document).ready(function() {
 				$('#text').val(ind);
 				$('#title').val($(this).text());
 				$('#html').val(data.content);
+				$('#image').val(data.img);
+				$('.slider img').attr('src', data.img);
 
 				if (data.date) { //if date field exists, set it as value, otherwise set current date by default
 					$('#datestamp').text(data.date);
@@ -126,49 +129,51 @@ $(document).ready(function() {
 
 	//add button event handler
 	$('#add-btn').on('click', function() {
-		var tabsBarWidth = parseInt($('.tabs').css('width'), 10),
-			tabWidth = parseInt($('.tab').css('width'), 10),
-			tabsCount = $('.tab').length;
+		var tabsBarWidth = parseInt($('.tabs').css('width'), 10), //get width of 'ul'
+			tabWidth = parseInt($('.tab').css('width'), 10), //get width of single tab
+			tabsCount = $('.tab').length; // get tabs count
 
-		if (tabsCount * tabWidth - (16 * (tabsCount - 5)) >= tabsBarWidth) {
+		if ((tabsCount + 1) * tabWidth - 16 * (tabsCount + 1) >= tabsBarWidth) { // if sum of length of all tabs + 1 is bigger than 'ul' width, we don't add one more
 			asideAddMsg('danger', 'Max length has been reached!');
 			return;
 		}
-		var item = $('.tab:first').clone();
-		if (!addItem(item)) {
+		var item = $('.tab:first').clone(); //clone first tab
+		if (!addItem(item)) { //try to add cloned tab
 			return;
 		}
 		// tabOffset();
 
-		var content = $('#html').val(),
-			date = $('#datepicker').val();
+		var content = $('#html').val(), //get tab data from inputs
+			date = $('#datepicker').val(),
+			img = $('#image').val();
 
-		item.removeClass('active');
-		tabsContentSize++;
-		tabsContent.set(item[0], {
+		item.removeClass('active'); //if cloned tab has 'active' class, we remove it
+		tabsContentSize++; //increment tabs count
+		tabsContent.set(item[0], { //set tab data to array
 			'content': content,
 			'date': date,
-			'img': null
+			'img': img
 		});
 
-		resetInputs();
+		resetInputs(); //clear inputs
 
 		asideAddMsg('success', 'Tab successfully added!');
 	});
 
 	//edit button event
 	$('#edit-btn').on('click', (e) => {
-		var item = $('.tab.active');
-		if (!addItem(item)) {
+		var item = $('.tab.active'); //get current tab
+		if (!addItem(item)) { // try to add item with entered index
 			return;
 		}
-		var data = tabsContent.get(item[0]);
+		var data = tabsContent.get(item[0]); // change saved tab's data
 		data.content = $('#html').val();
 		data.date = $('#datepicker').val();
-		var event = new Event("click", {
+		data.img = $('#image').val();
+
+		item[0].dispatchEvent(new Event("click", { //call 'click' event on edited tab
 			bubbles: true
-		});
-		item[0].dispatchEvent(event);
+		}));
 		// tabOffset();
 		asideAddMsg('success', 'Tab successfully edited!');
 	});
@@ -177,54 +182,53 @@ $(document).ready(function() {
 	$('a.collapse').on('click', function() {
 		var isCollapsed = false;
 		return function() {
-			if (isCollapsed) {
-				$('aside').animate({
+			if (isCollapsed) { //if sidebar is collapsed
+				$('aside').animate({ //we pull it to the right
 					'left': '0'
 				}, 300);
-				$('main').animate({
+				$('main').animate({ // and set left margin to main block
 					'margin-left': '500px'
 				}, 300);
-				$('a.collapse').html('&#8656;');
-				isCollapsed = !isCollapsed;
+				$('a.collapse').html('&#8656;'); // set mirrored arrow
+				isCollapsed = !isCollapsed; //set flag to false
 			} else {
-				$('aside').animate({
+				$('aside').animate({ //else we pull sidebar to the left
 					'left': '-455px'
 				}, 300);
-				$('main').animate({
+				$('main').animate({ //and remove margin of main block
 					'margin-left': '45px'
 				}, 300);
-				$('a.collapse').html('&#8658;');
+				$('a.collapse').html('&#8658;'); //and set default arrow
 				isCollapsed = !isCollapsed;
 			}
 		};
 	}());
 
-
 	function moveLeft() {
-		var currTab = $('.tab.active');
-		var index = currTab.index('.tab');
-		if (index === 0) {
-			index = tabsContentSize - 1;
+		var currTab = $('.tab.active'); //get current tab
+		var index = currTab.index('.tab'); //get index of current tab
+		if (index === 0) { //check tab's position
+			index = tabsContentSize - 1; // if it is first tab, set index to last tab
 		} else {
-			index--;
+			index--; // else set index to previous tab
 		}
 
-		var nextTab = $('.tab').eq(index);
+		var nextTab = $('.tab').eq(index); //get next(previous) tab
 
-		currTab.removeClass('active');
+		currTab.removeClass('active'); //remove active class from current tab
 
-		$('.slider img').fadeToggle(150, 'swing', function() {
-			nextTab.addClass('active');
-			nextTab[0].dispatchEvent(new Event('click', {
+		$('.slider img').fadeToggle(150, 'swing', function() { //animate image change
+			nextTab.addClass('active'); //set active class to the next tab
+			nextTab[0].dispatchEvent(new Event('click', { //call click event
 				bubbles: true
 			}));
-			$(this).fadeToggle(150, 'swing', () => {
+			$(this).fadeToggle(150, 'swing', () => { //enable left slider button
 				$('a.left').removeClass('disabled');
 			});
 		});
 	}
 
-	function moveRight() {
+	function moveRight() { // this works similary to the 'moveLeft' fuction
 		var currTab = $('.tab.active');
 		var index = currTab.index('.tab');
 		if (index === tabsContentSize - 1) {
@@ -247,15 +251,15 @@ $(document).ready(function() {
 			});
 		});
 	};
-
+	//left slider button handler
 	$('a.left').on('click', function left(e) {
 		debugger;
-		$(this).addClass('disabled');
-		moveLeft();
+		$(this).addClass('disabled'); //disable left slider button
+		moveLeft(); //call slide left function
 	});
-
+	//right slider button handler
 	$('a.right').on('click', function right(e) {
-		$(this).addClass('disabled');
-		moveRight();
+		$(this).addClass('disabled'); //disable right slider button
+		moveRight(); //call slide right functoin
 	});
 });
